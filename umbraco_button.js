5de69ed830629f52e6f25a5b17ff0908b805d97b
DESKTOP-L2EBV6A\nickk <nickkent2014@gmail.com>
2022-06-21 16:16:47 +0100

    added basic

--- umbraco_button.js
+++ umbraco_button.js
@@ -1,12 +1,7 @@
-document.querySelector(".container--home-content--title").textContent = "barnsley - hello"
-
 let umbraco_button = document.createElement("a")
-let my_header = document.querySelector(".masthead")
-let my_other_header = document.querySelector(".masthead__wrapper animated")
-
-umbraco_button.classList.add("umbraco-button")
-umbraco_button.textContent = "go to umbraco"
-// umbraco_button.attributes.add("href = www.google.com")
-
-// my_header.appendChild(umbraco_button)
-my_other_header.appendChild(umbraco_button)
+umbraco_button.textContent = "umbraco"
+umbraco_button.setAttribute("href", "/services")
+umbraco_button.style.color = "white"
+umbraco_button.style.margin = "4px"
+umbraco_button.style.textDecoration = "none"
+document.querySelector("div.masthead").appendChild(umbraco_button)
